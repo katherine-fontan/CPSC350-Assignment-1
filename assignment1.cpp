2200bdf2b4d5f0b56dc83191a8465553dfdc4aeb
katherine-fontan <luzfonta@chapman.edu>
2020-02-16 04:26:13 +0000

    reading from a file and count works + output file is created and writing to it works

--- assignment1.cpp
+++ assignment1.cpp
@@ -2,8 +2,18 @@
 #include <fstream>
 using namespace std;
 
-
 int main (int argc, char **argv){
+
+
+
+/*
+The program will accept as a command line argument
+the name of a text file that will contain an arbitrary list of DNA strings.
+(ie. ~/assign1/filename.txt) DNA strings consist of a sequence of nucleotides
+(A,C,T, or G). There will be 1 string per line of the file.
+No guarantees on capitalization.*/
+
+
 
 //getting input from command line argument
   //check command line argument
@@ -28,19 +38,40 @@ int main (int argc, char **argv){
     return 1; //error
   }
 
-  //reading from a file
+  int count = 0;
+
+
+  //while loop to read from a file
   while(!inFS.eof()){
+
     inFS >> fileInfo;
+
     if(!inFS.fail()){
-      cout << "string ->    "<< fileInfo <<endl;
+      //for loop to get each char of the file count how many strings there are in the file
+      for(int i = 0; i < fileInfo.size(); ++i)
+      {
+        char strInput = toupper(fileInfo[i]); //toupper function to account for lower and upper case letters
+        cout<< strInput<<endl;
+        count++; //add each time to count
+      }
     }
+
   }
-	inFS.close();
+
+  cout<<count<<endl; // check if count is correct
+	inFS.close(); //close input file stream
 
 
 
+  //math stuff idk how to do YET
+  /*
+  MATH/STATS THINGS TO COMPUTE
+  The program will then compute the sum, mean, variance,
+  and standard deviation of the length of the DNA strings in the list.
+  It will also compute the relative probability of each nucleotide (A,C,T, or G),
+   as well as the probability of each nucleotide bigram
+   (AA, AC, AT, AG, CA, CC, CT, CG, etc) across the entire collection.*/
 
-  //first work on reading from a file
   //then work on counting DNA string
   //then probability
 
@@ -51,7 +82,26 @@ int main (int argc, char **argv){
   //D floating point
 
 
+/* FILE OUTPUT
+The program will output the labeled results to a file called yourname.out.
+At the top of the file, output your name, student id, etc.
+After printing the summary statistics to yourname.out
+Append the 1000 strings to the end of yourname.out.*/
 
+ofstream outputFile; //object created **fout**
+outputFile.open("katherinefontan.txt");
+
+if (!outputFile.is_open()) {
+  cout << "Could not open file myoutfile.txt." << endl;
+  return 1;
+
+}
+else{
+  outputFile<<"Katherine Fontan \n2290561\nluzfonta@chapman.edu\nCPSC350-Assignment 1\n\n" <<endl; //contents to be written at the top of the file
+}
+outputFile<< "Summary\n"<<endl; //append to the file
+
+outputFile.close();
 
   return 0;
 }
